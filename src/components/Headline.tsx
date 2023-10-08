af5981f62f86c28d5b48e252c9389e8e7ab028cd
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-08 21:44:15 +0300

    Inceeased gap space.

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Headline = () => {
     return (
         <section className='flex 2xl:flex-row flex-col'>
-            <h1 className='text-[40px] text-Very-dark-blue font-extrabold leading-none'>The Bright Future of Web 3.0?</h1>
-            <div className='flex flex-col gap-3'>
+            {/* <h1 className='text-[40px] text-Very-dark-blue font-extrabold leading-none'>The Bright Future of Web 3.0?</h1> */}
+            <div className='flex flex-col gap-6'>
                 <p className='text-[15px] text-Dark-grayish-blue leading-[26px]'>
                     We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. 
                     But is it really fulfilling its promise?

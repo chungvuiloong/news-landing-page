ffb08a288a40b7998a4398231cef8c4f0d19127b
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-08 21:54:16 +0300

    Added image.

--- src/components/HeadLineImage.tsx
+++ src/components/HeadLineImage.tsx
@@ -1,9 +1,11 @@
+import Image from 'next/image';
 import React from 'react';
+import Headline_Img from '@/assets/images/image-web-3-mobile.jpg'
 
 const HeadLineImage = () => {
     return (
         <section>
-            
+            <Image src={Headline_Img} alt='Headline Image' />
         </section>
     );
 };

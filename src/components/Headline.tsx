6b0364c6f0a12ad6497ae9aa7ea6bc292167c2e5
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-08 21:15:22 +0300

    Add READ MORE btn

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -9,6 +9,7 @@ const Headline = () => {
                     We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. 
                     But is it really fulfilling its promise?
                 </p>
+                <button className='text-white'>READ MORE</button>
             </div>
         </section>
     );

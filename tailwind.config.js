8214d4fa4b0e09b35e5efdbdb26f7ae9bd16794d
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-08 09:29:54 +0300

    Add custom color.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      //  Primary
+      'Soft-orange': 'hsl(35, 77%, 62%)',
+      'Soft-red': 'hsl(5, 85%, 63%)',
+
+      // Neutral
+      'Off-white': 'hsl(36, 100%, 99%)',
+      'Grayish-blue': 'hsl(233, 8%, 79%)',
+      'Dark-grayish-blue': 'hsl(236, 13%, 42%)',
+      'Very-dark-blue': 'hsl(240, 100%, 5%)',
+    },
     screens: {
       // Mobile
       'sm': '375px',

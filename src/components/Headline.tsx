85ac08e0787efdf9f3e9d71753a16ce03402f6d6
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-08 21:46:49 +0300

    Added gap space between headline and content.

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Headline = () => {
     return (
-        <section className='flex 2xl:flex-row flex-col'>
-            {/* <h1 className='text-[40px] text-Very-dark-blue font-extrabold leading-none'>The Bright Future of Web 3.0?</h1> */}
+        <section className='flex 2xl:flex-row flex-col gap-y-3'>
+            <h1 className='text-[40px] text-Very-dark-blue font-extrabold leading-none'>The Bright Future of Web 3.0?</h1>
             <div className='flex flex-col gap-6'>
                 <p className='text-[15px] text-Dark-grayish-blue leading-[26px]'>
                     We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. 

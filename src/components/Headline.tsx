c3f8f4f60793a888361e0c73d728d827b4b4d189
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-08 21:11:17 +0300

    Added p tag and text.

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -4,7 +4,12 @@ const Headline = () => {
     return (
         <section className='flex 2xl:flex-row flex-col'>
             <h1 className='text-[40px] text-Very-dark-blue font-extrabold leading-none'>The Bright Future of Web 3.0?</h1>
-            
+            <div>
+                <p className='text-[15px] text-Dark-grayish-blue'>
+                    We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. 
+                    But is it really fulfilling its promise?
+                </p>
+            </div>
         </section>
     );
 };
@@ -13,7 +18,6 @@ export default Headline;
 
 // 
 
-// We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. 
-// But is it really fulfilling its promise?
+// 
 
 // Read more

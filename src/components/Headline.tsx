282f9011a2fed5584a9c890d0d1c921141b2a786
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-08 21:08:37 +0300

    Added h1 tag.

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 const Headline = () => {
     return (
-        <section>
+        <section className='flex 2xl:flex-row flex-col'>
+            <div className='text-[40px] text-Very-dark-blue font-extrabold leading-none'>The Bright Future of Web 3.0?</div>
             
         </section>
     );
 };
 
 export default Headline;
+
+// 
+
+// We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. 
+// But is it really fulfilling its promise?
+
+// Read more

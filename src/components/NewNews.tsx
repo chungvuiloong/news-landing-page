bf56ece3d0b16b29b9b654490bf3f812467a6581
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-08 12:51:50 +0300

    Made News component.

---
+++ src/components/NewNews.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+type NewsItem = {
+    heading: string;
+    paragraph: string;
+  };
+  
+const news_data: NewsItem[] = [
+    {
+        heading: 'Hydrogen VS Electric Cars',
+        paragraph: 'Will hydrogen-fueled cars ever catch up to EVs?'
+    },
+    {
+        heading: 'The Downsides of AI Artistry',
+        paragraph: 'What are the possible adverse effects of on-demand AI image generation?'
+    },
+    {
+        heading: 'Is VC Funding Drying Up?',
+        paragraph: 'Private funding by VC firms is down 50% YOY. We take a look at what that means.'
+    }
+]
+
+const NewNews = () => {
+    return (
+        <section className='p-5 bg-Very-dark-blue'>
+            <h2 className='text-Soft-orange font-bold'>New</h2>
+            <div>
+                {
+                    news_data.map((data, i) =>
+                    <div key={i}>
+                        <h3>{data.heading}</h3>
+                        <p className='text-[15px]'>{data.paragraph}</p>
+                    </div>
+                )}
+            </div>    
+        </section>
+    );
+};
+
+export default NewNews;

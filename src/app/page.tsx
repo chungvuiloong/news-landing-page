89446f366d1a2da4401fb96c7d1bdd03d775bd02
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-09 20:57:06 +0300

    Switched from flexbox to grid.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,17 @@ import TopThree from '@/components/TopThree'
 export default function Home() {
   return (
     <main>
-        <div className='container mx-auto flex flex-col'>
-            <div className='flex 2xl:flex-row flex-col gap-9'>
-                <div className='basis-2/3 flex flex-col 2xl:gap-y-9 gap-y-6'>
+        <div className='container mx-auto'>
+            <div className='grid md:grid-cols-3 grid-cols-1'>
+
                     <HeadLineImage />
                     <Headline />
-                </div>
-                <div className='basis-1/3'>
+
+                {/* <div className='basis-1/3'>
                     <NewPost />
-                </div>
+                </div> */}
             </div>
-            <TopThree />
+            {/* <TopThree /> */}
         </div>
     </main>
   )

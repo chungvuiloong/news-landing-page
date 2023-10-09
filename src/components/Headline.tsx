ff23b694ed3a6fe69dd1d90bba721f66eb3166ec
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-09 21:48:45 +0300

    Made headliner appear second row & occupy 2 span.

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Headline = () => {
     return (
-        <section className='flex 2xl:flex-row flex-col gap-y-3 gap-x-9'>
-            <h1 className='basis-1/2 2xl:text-6xl text-[40px] text-Very-dark-blue font-extrabold leading-none'>The Bright Future of Web 3.0?</h1>
-            <div className='basis-1/2 flex flex-col gap-6'>
+        <section className='row-start-2 col-span-2'>
+            <h1 className='2xl:text-6xl text-[40px] text-Very-dark-blue font-extrabold leading-none'>The Bright Future of Web 3.0?</h1>
+            <div className='flex flex-col gap-6'>
                 <p className='text-[15px] text-Dark-grayish-blue leading-[26px]'>
                     We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. 
                     But is it really fulfilling its promise?

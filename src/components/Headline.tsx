a30d8c3554ce22b58b3df511200c9b6e315b4bb0
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-10 11:23:35 +0300

    Made both components gap same as design.

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -6,7 +6,7 @@ const Headline = () => {
             <div className='row-start-2 col-span-1'>
                 <h1 className='2xl:text-6xl text-[40px] text-Very-dark-blue font-extrabold leading-none'>The Bright Future of Web 3.0?</h1>
             </div>
-            <div  className='row-start-2 col-span-1'>
+            <div  className='mb-8 row-start-2 col-span-1'>
                 <div className='h-full flex flex-col justify-between gap-6'>
                     <p className='text-[15px] text-Dark-grayish-blue leading-[26px]'>
                         We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. 

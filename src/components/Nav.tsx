2aab2a7ee0017c94599e17179c7f2cc92094b33a
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-10 13:09:34 +0300

    Added nav and text.

---
+++ src/components/Nav.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>New</li>
+                <li>Popular</li>
+                <li>Trending</li>
+                <li>Categories</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;

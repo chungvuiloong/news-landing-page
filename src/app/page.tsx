13b5f059d6b26cb8b5692528548ccdddf94d1d6c
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-10 15:21:00 +0300

    Removed unused code. Import Nav.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import HeadLineImage from '@/components/HeadLineImage'
 import Headline from '@/components/Headline'
-// import HeadlineParagraph from '@/components/HeadlineParagraph'
+import Nav from '@/components/Nav'
 import NewPost from '@/components/NewPost'
 import TopThree from '@/components/TopThree'
 
@@ -9,42 +9,14 @@ export default function Home() {
   return (
     <main>
         <div className='container mx-auto'>
+            <Nav />
             <div className='2xl:grid grid-cols-3 flex flex-col 2xl:gap-8 gap-0'>
-
-                    <HeadLineImage />
-                    <Headline />
-                    
-                  
-                    
-                    <NewPost />
-                     <TopThree />
-
-                {/* <div className='basis-1/3'>
-                    <NewPost />
-                </div> */}
+                <HeadLineImage />
+                <Headline />
+                <NewPost />
+                <TopThree />
             </div>
-            {/* <TopThree /> */}
         </div>
     </main>
   )
 }
-
-// Home
-// New
-// Popular
-// Trending
-// Categories
-
-
-
-// New 
-
-// Hydrogen VS Electric Cars
-// Will hydrogen-fueled cars ever catch up to EVs?
-
-// The Downsides of AI Artistry
-// What are the possible adverse effects of on-demand AI image generation?
-
-// Is VC Funding Drying Up?
-// Private funding by VC firms is down 50% YOY. We take a look at what that means.
-

ec4a5ef660481c4859cd89b759bfbf13bbc49581
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-10 16:36:21 +0300

    Put nav links into string array. Refactor code.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,16 +2,20 @@ import Image from 'next/image';
 import React from 'react';
 import Logo from '@/assets/images/logo.svg'
 
+const nav_link:String[] = ['Home', 'New', 'Popular', 'Trending', 'Categories']
+
 const Nav = () => {
     return (
         <nav className='2xl:pt-[90px] pt-7 flex justify-between'>
             <Image src={Logo} alt='Website logo' className='h-auto 2xl:w-auto w-[50%]'/>
             <ul className='flex gap-10 text-end text-Dark-grayish-blue'>
-                <li className='self-center hover:text-Soft-red cursor-pointer'>Home</li>
-                <li className='self-center hover:text-Soft-red  cursor-pointer'>New</li>
-                <li className='self-center hover:text-Soft-red  cursor-pointer'>Popular</li>
-                <li className='self-center hover:text-Soft-red  cursor-pointer'>Trending</li>
-                <li className='self-center hover:text-Soft-red  cursor-pointer'>Categories</li>
+                {
+                    nav_link.map((nav, i)=> 
+                        <li key={i} className='self-center hover:text-Soft-red cursor-pointer'>
+                            {nav}
+                        </li>
+                    )
+                }
             </ul>
         </nav>
     );

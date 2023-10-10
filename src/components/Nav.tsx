a288454f7f0a44308e1cb228fedc561674dddaf5
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-10 16:20:23 +0300

    Added padding-top. Added align self to nav links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,14 +4,14 @@ import Logo from '@/assets/images/logo.svg'
 
 const Nav = () => {
     return (
-        <nav className='flex justify-between'>
+        <nav className='2xl:pt-[90px] pt-7 flex justify-between'>
             <Image src={Logo} alt='Website logo' />
             <ul className='flex gap-10 text-end text-Dark-grayish-blue'>
-                <li>Home</li>
-                <li>New</li>
-                <li>Popular</li>
-                <li>Trending</li>
-                <li>Categories</li>
+                <li className='self-center'>Home</li>
+                <li className='self-center'>New</li>
+                <li className='self-center'>Popular</li>
+                <li className='self-center'>Trending</li>
+                <li className='self-center'>Categories</li>
             </ul>
         </nav>
     );

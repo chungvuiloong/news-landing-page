0561a43741a9abb80bf6d6803c3bca7623306756
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-10 16:33:08 +0300

    Added hover colour.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,11 +7,11 @@ const Nav = () => {
         <nav className='2xl:pt-[90px] pt-7 flex justify-between'>
             <Image src={Logo} alt='Website logo' className='h-auto 2xl:w-auto w-[50%]'/>
             <ul className='flex gap-10 text-end text-Dark-grayish-blue'>
-                <li className='self-center'>Home</li>
-                <li className='self-center'>New</li>
-                <li className='self-center'>Popular</li>
-                <li className='self-center'>Trending</li>
-                <li className='self-center'>Categories</li>
+                <li className='self-center hover:text-Soft-red cursor-pointer'>Home</li>
+                <li className='self-center hover:text-Soft-red  cursor-pointer'>New</li>
+                <li className='self-center hover:text-Soft-red  cursor-pointer'>Popular</li>
+                <li className='self-center hover:text-Soft-red  cursor-pointer'>Trending</li>
+                <li className='self-center hover:text-Soft-red  cursor-pointer'>Categories</li>
             </ul>
         </nav>
     );

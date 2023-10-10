89183b3f034953e8d3104cefc61e2939d3fe1c78
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-10 16:28:05 +0300

    Made logo responsive.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,7 +5,7 @@ import Logo from '@/assets/images/logo.svg'
 const Nav = () => {
     return (
         <nav className='2xl:pt-[90px] pt-7 flex justify-between'>
-            <Image src={Logo} alt='Website logo' />
+            <Image src={Logo} alt='Website logo' className='h-auto 2xl:w-auto w-[50%]'/>
             <ul className='flex gap-10 text-end text-Dark-grayish-blue'>
                 <li className='self-center'>Home</li>
                 <li className='self-center'>New</li>

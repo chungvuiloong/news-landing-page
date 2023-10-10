2cd8a286355521a7d1881c0324389fda70587fee
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-10 13:04:27 +0300

    Add margin for image.

--- src/components/HeadLineImage.tsx
+++ src/components/HeadLineImage.tsx
@@ -5,7 +5,7 @@ import Desktop_Headline_Img from '@/assets/images/image-web-3-desktop.jpg'
 
 const HeadLineImage = () => {
     return (
-        <section className='mt-8 row-start-1 col-span-2 w-full h-full'>
+        <section className='mt-8 2xl:mb-0 mb-6 row-start-1 col-span-2 w-full h-full'>
             <Image src={Desktop_Headline_Img} alt='Headline Image' className=''/>
         </section>
     );

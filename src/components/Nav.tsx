1f612665e9188d5bf30061bfcb21caab1b7df598
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-10 15:31:39 +0300

    Import image.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,11 @@
+import Image from 'next/image';
 import React from 'react';
+import Logo from '@/assets/images/logo.svg'
 
 const Nav = () => {
     return (
         <nav className='flex justify-between'>
-            <div>Image</div>
+            <Image src={Logo} alt='Website logo' />
             <ul className='flex gap-10 text-end text-Dark-grayish-blue'>
                 <li>Home</li>
                 <li>New</li>

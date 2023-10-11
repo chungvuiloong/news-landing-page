38ccb889373c152a085b35a0fedc3a55215aaaa6
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-11 15:51:00 +0300

    Used new lauout for navbar.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,34 +10,46 @@ const Nav = () => {
     const [modal, setModal] = useState(false)
 
     return (
-        <nav className='2xl:pt-[90px] pt-7 2xl:pb-7 pb-0 flex justify-between'>
-            <div>
-                <Image src={Logo} alt='Website logo' className='2xl:h-[40px] h-[30px] w-auto '/>
-            </div>
-                {
-                    modal ? <div className='min-h-screen fixed bg-Soft-red'>Open</div> : 
-                    <div className='h-full my-auto 2xl:hidden block' onClick={()=> setModal(!modal)}>
-                        <Image src={Hamburger_logo} alt='Hamburger Logo' className='cursor-pointer'/>
-                    </div>
-                }
-            {/* <div className='h-full my-auto 2xl:hidden block' onClick={()=> setModal(!modal)}>
-                {
-                    modal ? <div className='min-h-screen fixed bg-Soft-red'>Open</div> : 
-                        <Image src={Hamburger_logo} alt='Hamburger Logo' className='cursor-pointer'/>
-                }
-                <Image src={Hamburger_logo} alt='Hamburger Logo' className='cursor-pointer'/>
-            </div> */}
-            <ul className='2xl:flex hidden gap-10 text-end text-Dark-grayish-blue'>
-                {
-                    nav_link.map((nav, i)=> 
-                        <li key={i} className='self-center hover:text-Soft-red cursor-pointer'>
-                            {nav}
-                        </li>
-                    )
-                }
-            </ul>
+        <section className='2xl:pt-[90px] pt-7 2xl:pb-7 pb-0'>
+            <nav className='flex justify-between'>            
+                <div>
+                    <Image src={Logo} alt='Website logo' className='2xl:h-[40px] h-[30px] w-auto '/>
+                </div>            
+                <div className='flex justify-between'>
+                    {
+                        modal ? 
+                        <div className='min-h-screen fixed bg-Soft-red' onClick={()=> setModal(!modal)}>
+                            Open
+                        </div>
+                         : 
+                        <div className='my-auto 2xl:hidden block' onClick={()=> setModal(!modal)}>
+                            <Image src={Hamburger_logo} alt='Hamburger Logo' className='cursor-pointer'/>
+                        </div>
+                    }
+                {/* <div className='h-full my-auto 2xl:hidden block' onClick={()=> setModal(!modal)}>
+                    {
+                        modal ? <div className='min-h-screen fixed bg-Soft-red'>Open</div> : 
+                            <Image src={Hamburger_logo} alt='Hamburger Logo' className='cursor-pointer'/>
+                    }
+                    <Image src={Hamburger_logo} alt='Hamburger Logo' className='cursor-pointer'/>
+                </div> */}
+                <ul className='2xl:flex hidden gap-10 text-end text-Dark-grayish-blue'>
+                    {
+                        nav_link.map((nav, i)=> 
+                            <li key={i} className='self-center hover:text-Soft-red cursor-pointer'>
+                                {nav}
+                            </li>
+                        )
+                    }
+                </ul>
 
-        </nav>
+            </div>
+            
+            </nav>                
+
+
+        </section>
+
     );
 };
 

abe7a319926468ed59fb8df50f22642db054dbf1
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-11 07:23:55 +0300

    Made a test modal.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,22 +1,32 @@
+'use client'
 import Image from 'next/image';
-import React from 'react';
+import React, { useState} from 'react';
 import Logo from '@/assets/images/logo.svg'
 import Hamburger_logo from '@/assets/images/icon-menu.svg'
 
 const nav_link:String[] = ['Home', 'New', 'Popular', 'Trending', 'Categories']
 
 const Nav = () => {
+    const [modal, setModal] = useState(false)
+
     return (
         <nav className='2xl:pt-[90px] pt-7 2xl:pb-7 pb-0 flex justify-between'>
             <div>
                 <Image src={Logo} alt='Website logo' className='2xl:h-[40px] h-[30px] w-auto '/>
             </div>
-            <div className='h-full my-auto 2xl:hidden block'>
+                {
+                    modal ? <div className='min-h-screen fixed bg-Soft-red'>Open</div> : 
+                    <div className='h-full my-auto 2xl:hidden block' onClick={()=> setModal(!modal)}>
+                        <Image src={Hamburger_logo} alt='Hamburger Logo' className='cursor-pointer'/>
+                    </div>
+                }
+            {/* <div className='h-full my-auto 2xl:hidden block' onClick={()=> setModal(!modal)}>
+                {
+                    modal ? <div className='min-h-screen fixed bg-Soft-red'>Open</div> : 
+                        <Image src={Hamburger_logo} alt='Hamburger Logo' className='cursor-pointer'/>
+                }
                 <Image src={Hamburger_logo} alt='Hamburger Logo' className='cursor-pointer'/>
-                <div className='hidden'>
-                    Modal test
-                </div>
-            </div>
+            </div> */}
             <ul className='2xl:flex hidden gap-10 text-end text-Dark-grayish-blue'>
                 {
                     nav_link.map((nav, i)=> 
@@ -26,6 +36,7 @@ const Nav = () => {
                     )
                 }
             </ul>
+
         </nav>
     );
 };

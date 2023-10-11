b19c5b5b3e2b13de2f99b92d3cb99f0779c14a7c
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-11 07:01:46 +0300

    add hamburger logo. Add padding botton to nav.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,19 @@
 import Image from 'next/image';
 import React from 'react';
 import Logo from '@/assets/images/logo.svg'
+import Hamburger_logo from '@/assets/images/icon-menu.svg'
 
 const nav_link:String[] = ['Home', 'New', 'Popular', 'Trending', 'Categories']
 
 const Nav = () => {
     return (
-        <nav className='2xl:pt-[90px] pt-7 flex justify-between'>
-            <Image src={Logo} alt='Website logo' className='h-auto 2xl:w-auto w-[50%]'/>
+        <nav className='2xl:pt-[90px] pt-7 2xl:pb-7 pb-0 flex justify-between'>
+            <div>
+                <Image src={Logo} alt='Website logo' className='2xl:h-[40px] h-[30px] w-auto '/>
+            </div>
+            <div className='h-full my-auto 2xl:hidden block'>
+                <Image src={Hamburger_logo} alt='Hamburger Logo' className=''/>
+            </div>
             <ul className='2xl:flex hidden gap-10 text-end text-Dark-grayish-blue'>
                 {
                     nav_link.map((nav, i)=> 

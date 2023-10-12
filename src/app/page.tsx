6f68cc601c97f59fdf5f639a656bba11bd504737
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-12 16:35:57 +0300

    Added footer and pb.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer'
 import HeadLineImage from '@/components/HeadLineImage'
 import Headline from '@/components/Headline'
 import Nav from '@/components/Nav'
@@ -10,13 +11,14 @@ export default function Home() {
     <main>
         <Nav />
         <div className='container mx-auto'>
-            <div className='2xl:grid grid-cols-3 flex flex-col 2xl:gap-8 gap-0'>
+            <div className='2xl:pb-[8rem] pb-[5rem] 2xl:grid grid-cols-3 flex flex-col 2xl:gap-8 gap-0'>
                 <HeadLineImage />
                 <Headline />
                 <NewPost />
-                <TopThree />
+                <TopThree /> 
             </div>
         </div>
+        <Footer />
     </main>
   )
 }
